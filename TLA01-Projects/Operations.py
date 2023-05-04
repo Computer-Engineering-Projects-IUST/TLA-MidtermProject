0cf710d688fe3082433b7f80132fdc57284cae3a
Bahareh <kavousi.b99@gmail.com>
2023-05-04 23:24:41 +0330

    Added Union

--- TLA01-Projects/Operations.py
+++ TLA01-Projects/Operations.py
@@ -33,34 +33,17 @@ def Star (states, symbols, Trans, start_state, final_states):
     states.sort()
     print (states)
 
-    # ConnectToStart = frozendict({"":frozenset({start_state})})
-    # new_dict[New_Start_State] = ConnectToStart
     new_dict[New_Start_State] = AddLambda(New_Start_State, start_state, new_dict)
 
     for FinalState in final_states:
         try:                                        # already has a lambda transition
-            # temp = set(new_dict[FinalState][""])
-            # temp.add(New_Final_State)
-            # temp = frozenset(temp)
-            # ConnectFinalToFinal = frozendict({"":temp})
-            # new_dict[FinalState] = ConnectFinalToFinal
             new_dict[FinalState] = AppendLambda(FinalState, New_Final_State, new_dict)
         
         except:                                     # Does not have a lambda transition
-            # ConnectToFinal = frozendict({"":frozenset({New_Final_State})})
-            # new_dict[FinalState] = ConnectToFinal
             new_dict[FinalState] = AddLambda(FinalState, New_Final_State, new_dict)
 
-
-    # temp = set(new_dict[New_Start_State][""])
-    # temp.add(New_Final_State)
-    # temp = frozenset(temp)
-    # ConnectStartToFinal = frozendict({"":temp})
-    # new_dict[New_Start_State] = ConnectStartToFinal
     new_dict[New_Start_State] = AppendLambda(New_Start_State, New_Final_State, new_dict)
 
-    # ConnectFinalToStart = frozendict({"":frozenset({New_Start_State})})
-    # new_dict[New_Final_State] = ConnectFinalToStart
     new_dict[New_Final_State] = AddLambda(New_Final_State, New_Start_State, new_dict)
 
     new_frozendict = frozendict(new_dict)
@@ -68,8 +51,51 @@ def Star (states, symbols, Trans, start_state, final_states):
     print(new_frozendict)
 
 
-#def Star (states1, symbols, Trans1, start_state1, final_states1, states2, Trans2, start_state2, final_states2):
+# def Union (states1, symbols, Trans1, start_state1, final_states1, states2, Trans2, start_state2, final_states2):
+#     new_dict = dict(Trans)
 
+#     New_Start_State = 'q' + str(len(states))
+#     states.append(New_Start_State)
+#     states.sort()
+
+#     New_Final_State = 'q' + str(len(states))
+#     states.append(New_Final_State)
+#     states.sort()
+#     print (states)
+
+#     # ConnectToStart = frozendict({"":frozenset({start_state})})
+#     # new_dict[New_Start_State] = ConnectToStart
+#     new_dict[New_Start_State] = AddLambda(New_Start_State, start_state, new_dict)
+
+#     for FinalState in final_states:
+#         try:                                        # already has a lambda transition
+#             # temp = set(new_dict[FinalState][""])
+#             # temp.add(New_Final_State)
+#             # temp = frozenset(temp)
+#             # ConnectFinalToFinal = frozendict({"":temp})
+#             # new_dict[FinalState] = ConnectFinalToFinal
+#             new_dict[FinalState] = AppendLambda(FinalState, New_Final_State, new_dict)
+        
+#         except:                                     # Does not have a lambda transition
+#             # ConnectToFinal = frozendict({"":frozenset({New_Final_State})})
+#             # new_dict[FinalState] = ConnectToFinal
+#             new_dict[FinalState] = AddLambda(FinalState, New_Final_State, new_dict)
+
+
+#     # temp = set(new_dict[New_Start_State][""])
+#     # temp.add(New_Final_State)
+#     # temp = frozenset(temp)
+#     # ConnectStartToFinal = frozendict({"":temp})
+#     # new_dict[New_Start_State] = ConnectStartToFinal
+#     new_dict[New_Start_State] = AppendLambda(New_Start_State, New_Final_State, new_dict)
+
+#     # ConnectFinalToStart = frozendict({"":frozenset({New_Start_State})})
+#     # new_dict[New_Final_State] = ConnectFinalToStart
+#     new_dict[New_Final_State] = AddLambda(New_Final_State, New_Start_State, new_dict)
+
+#     new_frozendict = frozendict(new_dict)
+
+#     print(new_frozendict)
 
 if __name__ == '__main__':
     """ the main function for visualize the FA"""
