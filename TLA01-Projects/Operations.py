bd37b0b94e35b85c3b94a109ea5cb68f978fe454
Bahareh <kavousi.b99@gmail.com>
2023-05-06 01:42:40 +0330

    working on the bug

--- TLA01-Projects/Operations.py
+++ TLA01-Projects/Operations.py
@@ -6,6 +6,18 @@ from utils import read_fa, create_standard_fa
 from frozendict import frozendict
 
 
+def ChangeTransition (OldTransition, GoalState, Symbol):
+    #print(OldTransition)
+    new_set = frozenset({GoalState})
+    new_dict = frozendict({Symbol: new_set})
+    for key, value in OldTransition.items():
+        print(value)
+        print(new_dict)
+        OldTransition.remove(value)
+        OldTransition.update(new_dict)
+    return new_dict
+
+
 def AddLambda (Source, Dest, Dict):     # Does not have a lambda transition
     ConnectToSRC = frozendict({"":frozenset({Dest})})
     Dict[Source] = ConnectToSRC
@@ -19,6 +31,37 @@ def AppendLambda(Source, Dest, Dict):   # already has a lambda transition
     Dict[Source] = ConnectSRCToFinal
     return Dict[Source]
 
+
+def StarMain (): #Star
+    """ the main function for visualize the FA"""
+    args = sys.argv[1:]
+    #json_path = args[0]   # the relative path of the json file containing the dfa or nfa in the desired format
+    json_path = "samples/phase4-sample/star/in/FA.json"
+    try:
+            read_fa(json_path)
+            fa = create_standard_fa(1)
+            nfa = VisualNFA(fa)
+    except Exception as ex:
+        raise Exception("The input file is neither DFA nor NFA\nCheck whether you "
+                            "mentioned a correct file or its in the correct standard format")\
+            from ex
+
+    states=list(nfa.states)
+    states.sort()
+    #print(states)
+
+    symbols=list(nfa.input_symbols)
+    symbols.sort()
+    #print(symbols)
+    
+    Trans = nfa.transitions
+    #print(Trans)
+
+    start_state = nfa.initial_state
+    #print (f'starting state: {start_state}')
+    final_states = nfa.final_states
+    #print (final_states)
+    Star(states, symbols, Trans, start_state, final_states)
 
 
 def Star (states, symbols, Trans, start_state, final_states):
@@ -51,113 +94,13 @@ def Star (states, symbols, Trans, start_state, final_states):
     print(new_frozendict)
 
 
-def Union (states1, symbols1, Trans1, start_state1, final_states1, states2, symbols2, Trans2, start_state2, final_states2):
-    new_dict1 = dict(Trans1)
-    new_dict2 = dict(Trans2)
-    Result_dict = new_dict1.copy()
-
-    Result_States = states1.copy()
-    #print(states1)
-    # print(Result_States)
-    #print(new_dict2)
-    # print ('q' + str(len(states1)))
-    # Result_States.append('q' + str(len(states1)))
-    # print(Result_States)
-    new_dict2_copy = new_dict2.copy()
-    for state in states2:
-        New_state = 'q' + str(len(Result_States))
-        Result_States.append(New_state)
-        new_dict2_copy[New_state] = new_dict2[state]
-        #del new_dict2_copy[state]
-        # print(f'State is: {state} and New_state is: {New_state}')
-        #print(f'new_dict2[state] is: {new_dict2[state]}')
-    #     print(f'new_dict2_copy[New_state] is: {new_dict2_copy[New_state]}')
-    #     print("==================")
-    # print(new_dict2_copy)
-    # print(Result_States)
-    #print (states1)
-    for state in states1:
-        #print(state)
-        del new_dict2_copy[state]
-
-    #print(new_dict2_copy)
-    Result_dict.update(new_dict2_copy)
-    #print(Result_dict)
-    # New_Start_State = 'q' + str(len(states))
-    # states.append(New_Start_State)
-    # states.sort()
-
-    # New_Final_State = 'q' + str(len(states))
-    # states.append(New_Final_State)
-    # states.sort()
-    # print (states)
-
-    # # ConnectToStart = frozendict({"":frozenset({start_state})})
-    # # new_dict[New_Start_State] = ConnectToStart
-    # new_dict[New_Start_State] = AddLambda(New_Start_State, start_state, new_dict)
-
-    # for FinalState in final_states:
-    #     try:                                        # already has a lambda transition
-    #         # temp = set(new_dict[FinalState][""])
-    #         # temp.add(New_Final_State)
-    #         # temp = frozenset(temp)
-    #         # ConnectFinalToFinal = frozendict({"":temp})
-    #         # new_dict[FinalState] = ConnectFinalToFinal
-    #         new_dict[FinalState] = AppendLambda(FinalState, New_Final_State, new_dict)
-        
-    #     except:                                     # Does not have a lambda transition
-    #         # ConnectToFinal = frozendict({"":frozenset({New_Final_State})})
-    #         # new_dict[FinalState] = ConnectToFinal
-    #         new_dict[FinalState] = AddLambda(FinalState, New_Final_State, new_dict)
 
 
-    # # temp = set(new_dict[New_Start_State][""])
-    # # temp.add(New_Final_State)
-    # # temp = frozenset(temp)
-    # # ConnectStartToFinal = frozendict({"":temp})
-    # # new_dict[New_Start_State] = ConnectStartToFinal
-    # new_dict[New_Start_State] = AppendLambda(New_Start_State, New_Final_State, new_dict)
 
-    # # ConnectFinalToStart = frozendict({"":frozenset({New_Start_State})})
-    # # new_dict[New_Final_State] = ConnectFinalToStart
-    # new_dict[New_Final_State] = AddLambda(New_Final_State, New_Start_State, new_dict)
 
-    # new_frozendict = frozendict(new_dict)
 
-    # print(new_frozendict)
 
-if __name__ == '__main__':
-    #Star
-    # """ the main function for visualize the FA"""
-    # args = sys.argv[1:]
-    # #json_path = args[0]   # the relative path of the json file containing the dfa or nfa in the desired format
-    # json_path = "samples/phase4-sample/star/in/FA.json"
-    # try:
-    #         read_fa(json_path)
-    #         fa = create_standard_fa(1)
-    #         nfa = VisualNFA(fa)
-    # except Exception as ex:
-    #     raise Exception("The input file is neither DFA nor NFA\nCheck whether you "
-    #                         "mentioned a correct file or its in the correct standard format")\
-    #         from ex
-
-    # states=list(nfa.states)
-    # states.sort()
-    # #print(states)
-
-    # symbols=list(nfa.input_symbols)
-    # symbols.sort()
-    # #print(symbols)
-    
-    # Trans = nfa.transitions
-    # #print(Trans)
-
-    # start_state = nfa.initial_state
-    # #print (f'starting state: {start_state}')
-    # final_states = nfa.final_states
-    # #print (final_states)
-    # Star(states, symbols, Trans, start_state, final_states)
-    
+def UnionMain ():
     #Union
     """ the main function for visualize the FA"""
     args = sys.argv[1:]
@@ -190,13 +133,14 @@ if __name__ == '__main__':
 
     start_state1 = nfa1.initial_state
     #print (f'starting state1: {start_state1}')
-    final_states1 = nfa1.final_states
-    #print (final_states1)
+    #print(type(start_state1))
+    final_states1 = set(nfa1.final_states)
+    #print (type(final_states1))
 
 
     states2=list(nfa2.states)
     states2.sort()
-    #print(states2)
+    #print(type(states2))
 
     symbols2=list(nfa2.input_symbols)
     symbols2.sort()
@@ -207,7 +151,72 @@ if __name__ == '__main__':
 
     start_state2 = nfa2.initial_state
     #print (f'starting state2: {start_state2}')
-    final_states2 = nfa2.final_states
+    final_states2 = set(nfa2.final_states)
     #print (final_states2)
 
     Union(states1, symbols1, Trans1, start_state1, final_states1, states2, symbols2, Trans2, start_state2, final_states2)
+
+
+def Union (states1, symbols1, Trans1, start_state1, final_states1, states2, symbols2, Trans2, start_state2, final_states2):
+    new_dict1 = dict(Trans1)
+    new_dict2 = dict(Trans2)
+    Result_dict = new_dict1.copy()
+    Result_States = states1.copy()
+
+    New_Start_State = 'q' + str(len(states1) + len(states2))
+    Result_States.append(New_Start_State)
+    Result_States.sort()
+    Result_dict[New_Start_State] = AddLambda(New_Start_State, start_state1, Result_dict)
+    
+
+    New_Final_State = 'q' + str(len(states1) + len(states2) + 1)
+    Result_States.append(New_Final_State)
+    Result_States.sort()
+    #print (Result_States)
+    for FinalState in final_states1:
+        try:                                        # already has a lambda transition
+            Result_dict[FinalState] = AppendLambda(FinalState, New_Final_State, Result_dict)
+        
+        except:                                     # Does not have a lambda transition
+            Result_dict[FinalState] = AddLambda(FinalState, New_Final_State, Result_dict)
+
+
+    new_dict2_copy = new_dict2.copy()
+    changedStart2 = False
+    for state in states2:
+        New_state = 'q' + str(len(Result_States) - len(states2) + 1)
+        Result_States.append(New_state)
+        new_dict2_copy[New_state] = ChangeTransition(new_dict2[state], New_state, 'b')
+        #print(new_dict2_copy[New_state])
+        if state == start_state2 and changedStart2 == False:
+            #print(f'state is: {state} and start state2 is:  {start_state2} and new state is: {New_state}')
+            start_state2 = New_state
+            changedStart2 = True
+        if state in final_states2:
+            final_states2.remove(state)
+            final_states2.update(New_state)
+
+
+    for state in states1:
+        del new_dict2_copy[state]
+
+    
+    Result_dict.update(new_dict2_copy)
+    #print(f'Start State2: {start_state2}   and final state2: {final_states2}')
+    
+    Result_dict[New_Start_State] = AppendLambda(New_Start_State, start_state2, Result_dict)
+    for FinalState in final_states2:
+        try:                                        # already has a lambda transition
+            Result_dict[FinalState] = AppendLambda(FinalState, New_Final_State, Result_dict)
+        
+        except:                                     # Does not have a lambda transition
+            Result_dict[FinalState] = AddLambda(FinalState, New_Final_State, Result_dict)
+
+    #print(Result_dict)
+
+if __name__ == '__main__':
+    
+    #StarMain()
+    UnionMain()
+
+    
